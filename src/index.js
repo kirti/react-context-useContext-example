5f7f9c4ab5257ac34d3583e3c54da524167f3429
kirti kaushal <kirtikau@gmail.com>
2021-07-05 10:27:30 +0530

    add index provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeContext , themes} from './theme-context'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext.Provider value={themes.light}> 
+        <App />
+    </ThemeContext.Provider>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
